e7e013e6355d15acf0275f0c5a40a0d08fe4cd16
kyr1234 <yugkhokhar18@gmail.com>
2022-05-03 02:41:16 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,96 @@
+/* const GOOGLE_API_FOLDER_ID = '11sBL2sL1bmcDZ_fkzRnKhc_03t30EU6X'
+const fs = require('fs')
+const { google } = require('googleapis') */
+const multer=require('multer')
+const path=require('path')
+/* let value; */
+let express = require('express')
+const app = express()
+const cors = require('cors')
+let cookieParser = require('cookie-parser')
+let logger = require('morgan')
+const bodyParser = require('body-parser')
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true,
+}),
+)
+const  storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"Images")
+
+    },
+    filename:(req,file,cb)=>{
+/* value=Date.now()+path.extname(file.originalname) */
+        console.log(file)
+        cb(null,Date.now()+path.extname(file.originalname))
+    }
+})
+
+const upload =multer({storage:storage});
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('deployed')
+})
+
+app.get('/', (req, res) => {
+  res.send('Data Added Successfully')
+})
+
+
+/* const uploadFile=async(file) =>{
+    try{
+        const auth = new google.auth.GoogleAuth({
+            keyFile: './google.json',
+            scopes: ['https://www.googleapis.com/auth/drive']
+        })
+
+        const driveService = google.drive({
+            version: 'v3',
+            auth
+        })
+
+        const fileMetaData = {
+            'name': 'snowplace.jpg',
+            'parents': [GOOGLE_API_FOLDER_ID]
+        }
+
+        const media = {
+            mimeType: 'image/jpg',
+            body: fs.createReadStream(`./Images/${file}`)
+        }
+
+        const response = await driveService.files.create({
+            resource: fileMetaData,
+            media: media,
+            field: 'id'
+        })
+        return response.data.id
+
+    }catch(err){
+        console.log('Upload file error', err)
+    }
+} */
+
+
+
+
+
+
+/* 
+uploadFile(value).then(data => {
+    console.log(data)
+    //https://drive.google.com/uc?export=view&id=1Lf7ASG6D2wyuzSdgEhZf4epjvaPwn-R9
+}) */
